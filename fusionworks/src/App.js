5e4309fe5c69ffe44b07451b7025dbfb3ada4c22
sihabutheen8117 <sihabutheen8117@gmail.com>
2024-09-29 13:39:51 +0530

    cleaned react defaults

--- fusionworks/src/App.js
+++ fusionworks/src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="bg-red-400">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Welcome to fusion works</div>
     </div>
   );
 }
