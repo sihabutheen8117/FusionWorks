53231359957d2208a4f87632009a7ee4375be06b
sihabutheen8117 <sihabutheen8117@gmail.com>
2024-10-02 10:30:09 +0530

    frontend - project list template created

--- fusionworks/src/component/main/project/Project.js
+++ fusionworks/src/component/main/project/Project.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import ProjectList from './ProjectList.js'
+import { useState } from 'react'
+
 
 const Project = () => {
+
+
+  const [search ,setSearch] = useState("")
+  const [filter,setFilter]  = useState("recent")
 
 
   return (
     <div>
       <div className='container'>
-        <div>
-          <ProjectList/>
+
+        <div className='container flex flex-row'>
+          <input type="text" 
+          placeholder = "search"
+          onChange={(event)=>{
+            setSearch(event.target.value)
+          }}
+          className='border-2 border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none px-2 py-1'
+          />
+          <select value={filter} 
+          onChange={(event)=>{
+            setFilter(event.target.value)
+          }}>
+            <option value="old">old</option>
+            <option value="recent">recent</option>
+          </select>
+        </div>
+
+        
+        <ProjectList/>
+
+        {/* for adding the project for their own */}
+        <div className=''>
         </div>
       </div>
     </div>
--- fusionworks/src/component/main/project/ProjectList.js
+++ fusionworks/src/component/main/project/ProjectList.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const ProjectList = () => {
+
+
   return (
     <div>
-      inside project list
+
+      <div className='container'>
+        <div className='grid grid-cols-12 gap-3'>
+          <div className='col-span-8 text-white bg-slate-600'>subject</div>
+          <div className='col-span-2 text-white bg-slate-600'>person</div>
+          <div className='col-span-2 text-white bg-slate-600'>time ago</div>
+        </div>
+        <div className='grid grid-cols-12 gap-3'>
+          <div className='col-span-12 text-white bg-slate-600'>describtion</div>
+        </div>
+        <div className="grid grid-cols-12 gap-3">
+          <div className='col-span-1 text-white bg-slate-600'>by
+          </div>
+          <div className='col-span-3 text-white bg-slate-600'>sihabutheen</div>
+          <div className='col-span-2 text-white bg-slate-600'>members</div>
+          <div className='col-span-2 text-white bg-slate-600'> 5 member</div>
+          <div className='col-span-2 text-white bg-slate-600'></div>
+          <div className='col-span-2 text-white bg-slate-600'>apply</div>
+        </div>
+      </div>
+      
+      inside the project list
     </div>
   )
 }
