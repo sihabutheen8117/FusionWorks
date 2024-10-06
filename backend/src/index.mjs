fefda73ded7310004e4661aca342d3477ba61980
sihabutheen8117 <sihabutheen8117@gmail.com>
2024-10-06 15:47:35 +0530

    backend - imported passport

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import passport from 'passport'
 
 
 const app = express()
@@ -7,6 +8,10 @@ const PORT = process.env.PORT || 3000
 mongoose.connect('mongodb://localhost/fusionworks')
 .then(()=>console.log(`Connected to Database`))
 .catch((err)=>console.log(`Error : ${err}`))
+
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.listen(PORT , ()=>{
     console.log(`Running on PORT : ${PORT}`)
