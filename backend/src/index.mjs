1426245ebb4cffe590f05f30b2f710dd501fe727
sihabutheen8117 <sihabutheen8117@gmail.com>
2024-10-08 18:39:43 +0530

    backend : backage port changed to 3005

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,17 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import passport from 'passport'
 
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3005
 mongoose.connect('mongodb://localhost/fusionworks')
 .then(()=>console.log(`Connected to Database`))
 .catch((err)=>console.log(`Error : ${err}`))
 
-
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.listen(PORT , ()=>{
     console.log(`Running on PORT : ${PORT}`)
