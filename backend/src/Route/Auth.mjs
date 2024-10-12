df51ad320642994be2c6f1abbd83f0f93fe266b0
sihabutheen8117 <sihabutheen8117@gmail.com>
2024-10-12 14:07:09 +0530

    backend - authendication succeessfully

--- backend/src/Route/Auth.mjs
+++ backend/src/Route/Auth.mjs
@@ -1,101 +1,126 @@
-import { response, Router } from "express";
+import {  Router } from "express";
 import { User } from "../mongoose/schemas/user.mjs";
-import {body ,validationResult ,checkSchema } from 'express-validator'
+import {body, validationResult } from 'express-validator'
 import "../strategies/local-strategy.mjs"
 import passport from "passport";
 
 const routerAuth = Router();
 
-routerAuth.post("/api/register" ,
-    [
-        body('name')
-        .trim()
-        .notEmpty()
-        .withMessage("name cannot be empty")
-        .isLength({min : 3 , max :32})
-        .withMessage("name should be 3>= and 32<=")
-        .isString()
-        .withMessage("name should not contain numeric value"),
 
-        body('email')
-        .trim()
-        .notEmpty()
-        .withMessage("email cannot be empty")
-        .isEmail()
-        .withMessage("invalid mail address"),
+//registering 
+routerAuth.post("/api/register" ,async (req ,res)=>{
 
-        body('password')
-        .notEmpty()
-        .withMessage("password cannot be empty")
-        .isLength({min : 8 , max : 32})
-        .withMessage("password length should be 8<= and <=32")
-    ],
-    async (req ,res)=>{
-    const {name , email , password} = req.body ;
-    // const newuser = new User(req.body)
-    // const savedUser = await newuser.save()
-    console.log("from /api/register")
-    req.sessionStore.get(req.session.id ,(err ,user)=>{
-        if(err){
-            console.log(err)
+    const {body} = req ;
+
+    try{
+        const newuser = new User(body)
+        const savedUser = await newuser.save()
+        return res.status(200).send({
+            status : "registered successfully",
+            errors  : ""
+        })
+    }catch(error){
+
+         // Check if the error is a Mongoose validation error
+         if (error.name === 'ValidationError') {
+            // Extract the validation errors and send them to the user
+            const validationErrors = {} ;
+            Object.keys(error.errors).forEach((field) => {
+                validationErrors[field] = error.errors[field].message;
+            });
+            return res.status(400).json({ errors: validationErrors });
         }
-        else{
-            console.log(user)
+
+        // Check for duplicate key error (E11000)
+        if (error.code === 11000) {
+            const duplicateKey = Object.keys(error.keyValue)[0]; // Get the field causing the duplicate key error
+            const duplicateValue = error.keyValue[duplicateKey];
+            return res.status(400).json({
+                error: `Duplicate value for ${duplicateKey}: '${duplicateValue}' already exists. Please choose another one.`
+            });
         }
-    })
-    console.log(req.session);
-    console.log(req.session.id)
-    req.session.visited = true 
-    res.status(201).send({
-        name : name ,
-        email : email ,
-        password : password,
-        status : "registered",
-    })
+
+        // Handle other errors
+        return res.status(500).json({ message: 'Server error' , err : error.message });
+    }
+   
+    
 })
 
 
+
+//update register
 routerAuth.post("/api/register/update", async (req ,res)=>{
 
-    const {body}  = req ;
-    
+    if(!req.user){
+       return res.status(500).send({
+            error : "not authendicated"
+        })
+    }
+
+    const {body} = req;
+
+    try{
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user.id,
+            { $set : body },
+            { new: true, runValidators: true }
+        )
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json({
+            errors : "",
+            status : "profile updated successfully"
+        })
+    }catch (error){
+
+        if (error.name === 'ValidationError') {
+            // Extract the validation errors and send them to the user
+            const validationErrors = {} ;
+            Object.keys(error.errors).forEach((field) => {
+                validationErrors[field] = error.errors[field].message;
+            });
+            return res.status(400).json({ errors: validationErrors });
+        }
+
+        // Check for duplicate key error (E11000)
+        if (error.code === 11000) {
+            const duplicateKey = Object.keys(error.keyValue)[0]; // Get the field causing the duplicate key error
+            const duplicateValue = error.keyValue[duplicateKey];
+            return res.status(400).json({
+                error: `Duplicate value for ${duplicateKey}: '${duplicateValue}' already exists. Please choose another one.`
+            });
+        }
+
+        // Handle other errors
+        return res.status(500).json({ message: 'Server error' , err : error.message });
+        
+    }
+
 })
 
-routerAuth.post("/api/login" , [
-    body('email')
-        .trim()
-        .notEmpty()
-        .withMessage("email cannot be empty")
-        .isEmail()
-        .withMessage("invalid mail address"),
-
-        body('password')
-        .notEmpty()
-        .withMessage("password cannot be empty")
-        .isLength({min : 8 , max : 32})
-        .withMessage("password length should be 8<= and <=32")
-],
+//login
+routerAuth.post("/api/login" ,
 passport.authenticate("local")
-,async(req,res)=>{
-    console.log(req.session.id)
-
+,(req,res)=>{
+   
     res.status(200).send({
+        error : "",
         status : "successfully loggedIn"
     })
-})
+},
+(err, req, res, next) => {
+    // Handle unexpected errors
+    if (err) {
+        return res.status(500).json({  errors: err.message });
+    }
 
-
-routerAuth.get("/api/login/status" , (req,res)=>{
-    console.log("inside /api/login/status");
-    console.log(req.user)
-
-    return (req.user) ? res.status(200).send({
-        status : "logged in"
-    }) : res.status(401).send({
-        status : "not logged in"
-    })
-    
-})
+    // Handle authentication failure
+    res.status(401).json({ message: 'Authentication failed. Invalid credentials.' });
+}
+)
 
 
 export default routerAuth ;
---
+++ backend/src/mongoose/schemas/discussion.mjs
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose .Schema({
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true
+    },
+
+    message : {
+        type : mongoose.Schema.Types.String,
+        required : true
+    },
+
+    createdAt :{
+        type : Date,
+        default : Date.now
+    }
+})
+
+const dicussionSchema = mongoose.Schema ({
+    subject : {
+        type : mongoose.Schema.Types.String,
+        required:true 
+    },
+
+    describtion : {
+        type : mongoose.Schema.Types.String,
+        required : true 
+    },
+
+    replies : [messageSchema],
+
+    numberOfReplies : {
+        type : mongoose.Schema.Types.Number
+    },
+
+    creator : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true 
+    },
+
+    liked : {
+        type : [mongoose.Schema.Types.ObjectId],
+    },
+
+    numberOfLikes : {
+        type : mongoose.Schema.Types.Number ,
+        default : 0 
+    },
+
+    disliked : {
+        type : [mongoose.Schema.Types.ObjectId],
+    },
+
+    numberOfDisLikes : {
+        type : mongoose.Schema.Types.Number ,
+        default : 0 
+    },
+
+    createdAt : {
+        type: Date,
+        default: Date.now
+    }
+
+
+})
+
+dicussionSchema.pre('save' , (next)=> {
+    this.numberOfLikes = this.liked.length;
+    this.numberOfDisLikes = this.disliked.length;
+    this.numberOfReplies = this.replies.length;
+    next();
+})
+
+export const Discussion = mongoose.model('Disscussion' ,dicussionSchema)
