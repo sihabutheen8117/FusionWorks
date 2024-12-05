696aa0b692ddacba629b178b75531dbf2c2a5259
sihabutheen8117 <sihabutheen8117@gmail.com>
2024-12-05 18:22:58 +0530

    backend updated

--- fusionworks/src/feature/userPostApi.js
+++ fusionworks/src/feature/userPostApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const userPostApi = createApi({
     reducerPath : 'postApi',
     baseQuery : fetchBaseQuery({ 
-        baseUrl : 'http://localhost:3005/',
+        baseUrl :process.env.API_URL,
         credentials : 'include'
     }),
 
